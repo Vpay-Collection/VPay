31fdb3d5b91eb47641e69764c48790b1a112bac6
Dreamn <37787014+dreamncn@users.noreply.github.com>
2022-01-29 21:03:11 +0800

    Delete protected directory

--- protected/lib/email/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function($class)
-{
-    $class = str_replace('PHPMailer\\PHPMailer\\','email/', $class);
-    $file =APP_LIB .  $class . '.php';
-    //echo $file.'<br >';
-    if (file_exists($file)) include $file;
-
-}, true, true);
