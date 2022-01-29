4baf55ee913ff1f8cfedcd067e4481b5c183966e
ankio <dream@dreamn.cn>
2022-01-29 21:40:56 +0800

    fix: 删除部分提交

--- src/extend/ankioTask/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*******************************************************************************
- * Copyright (c) 2022. CleanPHP. All Rights Reserved.
- ******************************************************************************/
-
-use app\core\event\EventManager;
-//注册拓展运行位置
-EventManager::attach("afterFrameInit", 'app\extend\ankioTask\Main');
-
