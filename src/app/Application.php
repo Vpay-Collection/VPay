20e8a6bb6d7e80b699b3707ded2e1dfa74bf8833
ankio <admin@ankio.net>
2023-05-28 16:08:27 +0800

    pref:心跳监控还是调整为1小时

--- src/app/Application.php
+++ src/app/Application.php
@@ -73,7 +73,7 @@ EOF
 
 
         if(!TaskerManager::has("App心跳守护进程")){
-            TaskerManager::add(TaskerTime::nMinute(1),new DaemonTasker(),"App心跳守护进程",-1);
+            TaskerManager::add(TaskerTime::nHour(1,0),new DaemonTasker(),"App心跳守护进程",-1);
         }
 
 
