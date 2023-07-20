dc625432b494e370f3bb7114e559e60509570a5a
ankio <admin@ankio.net>
2023-07-20 23:04:41 +0800

    pref:优化版本依赖

--- src/app/config_example.php
+++ src/app/config_example.php
@@ -6,10 +6,9 @@
                     0 => '0.0.0.0',
                 ),
             'rewrite' => true,
-            'version' => '4.0.5',
-            'log' => 30,
-            'session' => 'pay_session',
-            'view_debug' => false,
+            'version' => '4.0.6',
+            'log' => 1,
+            'session' => 'pay_session'
         ),
 
     'route' =>
@@ -17,27 +16,13 @@
             'robots.txt' => 'api_index/main/robots',
             '/' => 'index/main/index',
             'admin' => 'admin/main/index',
-            'shop' => 'shop/main/index',
             'pay/<id>' => 'index/main/pay',
             'item/<id>' => 'shop/main/item',
             'order/<a>' => 'api/pay/<a>',
             'image/<type>/<file>' => 'api_index/main/image',
             'api/admin/<c>/<a>' => 'api_admin/<c>/<a>',
             'api/index/<c>/<a>' => 'api_index/<c>/<a>',
-            'api/shop/<c>/<a>' => 'api_shop/<c>/<a>',
-            'wechat/<c>/<a>' => 'api_wechat/<c>/<a>',
-            'api/user/<c>/<a>' => 'api_user/<c>/<a>',
             '<m>/<c>/<a>' => '<m>/<c>/<a>',
-        ),
-    'mail' =>
-        array (
-            'received' => '',
-            'smtp' => '',
-            'send' => '',
-            'passwd' => '',
-            'port' => 465,
-            'pay_success' => false,
-            'pay_daily' => false,
         ),
 
     'database' =>
@@ -53,31 +38,5 @@
                     'charset' => 'utf8mb4',
                 ),
         ),
-    'login' =>
-        array (
-            'username' => '',
-            'password' => '',
-            'image' => '',
-        ),
-    'shop' =>
-        array (
-            'state' => true,
-            'title' => 'Ankioの小站',
-            'notice' => '',
-            'host' => '',
-            'key' => '',
-            'time' => 5,
-            'id' => 1,
-        ),
-    'channel' =>
-        array (
-            'alipay' => '',
-            'wechat' => '',
-        ),
-    'app' =>
-        array (
-            'timeout' => 5,
-            'key' => '',
-            'conflict' => 1,
-        ),
+
 );
