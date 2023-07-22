55b0c5e4352d67636d66f0af39b7e63b1f5f5cb2
ankio <admin@ankio.net>
2023-07-22 14:22:23 +0800

    pref:修复SSO登录验证问题

---
+++ src/app/controller/api/Sign.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright (c) 2023. Ankio.  由CleanPHP4强力驱动。
+ */
+/**
+ * Package: app\controller\api
+ * Class Sign
+ * Created By ankio.
+ * Date : 2023/7/22
+ * Time : 11:57
+ * Description :
+ */
+
+namespace app\controller\api;
+
+class Sign extends BaseController
+{
+
+}
