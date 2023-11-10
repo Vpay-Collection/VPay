a0971f055c124f2e7818c67b42512772b8853741
Ankio <ankio@ankio.net>
2023-11-10 13:06:25 +0800

    :arrow_up: CleanPHP新版本配置项更新

--- src/app/config.example.php
+++ src/app/config.example.php
@@ -16,9 +16,7 @@
             'admin' => 'admin/main/index',
             'pay/<id>' => 'index/main/pay',
             'order/<a>' => 'api/pay/<a>',
-            'file/<file>' => 'api_index/main/file',
-            'api/admin/<c>/<a>' => 'api_admin/<c>/<a>',
-            'api/index/<c>/<a>' => 'api_index/<c>/<a>',
+            'file/<file>' => 'index/main/file',
             '<m>/<c>/<a>' => '<m>/<c>/<a>',
         ),
     'database' =>
